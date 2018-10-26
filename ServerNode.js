c568b7d0f25d79dff275ce4bd0343013cf0dfb4b
Tim Graupmann <tgraupmann@gmail.com>
2018-10-25 22:31:20 -0700

    Added parent folder

--- ServerNode.js
+++ ServerNode.js
@@ -7,6 +7,8 @@ app.use(function (req, res, next) {
 
 app.use(express.static('.'));
 
+app.use(express.static('..'));
+
 app.listen(1337, function () {
   console.log('Example app listening on port 1337!');
 })
