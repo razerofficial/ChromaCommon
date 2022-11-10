4326fb9b46d6f359bfbd9765a510e079a7c8e22c
Timothy Graupmann <tgraupmann@gmail.com>
2022-11-09 18:52:44 -0800

    Added device set support

--- ChromaSDKWS.js
+++ ChromaSDKWS.js
@@ -4703,25 +4703,18 @@ var ChromaAnimation = {
     if (animation == undefined) {
       return;
     }
-    if (animation.DeviceType != EChromaSDKDeviceTypeEnum.DE_2D ||
-      animation.Device != EChromaSDKDevice2DEnum.DE_Keyboard) {
-      return;
-    }
     var redOffset = (color & 0xFF);
     var greenOffset = (color & 0xFF00) >> 8;
     var blueOffset = (color & 0xFF0000) >> 16;
     var frames = animation.Frames;
-    var maxRow = ChromaAnimation.getMaxRow(EChromaSDKDevice2DEnum.DE_Keyboard);
-    var maxColumn = ChromaAnimation.getMaxColumn(EChromaSDKDevice2DEnum.DE_Keyboard);
-    //console.log(animation.Frames);
-    for (var frameId = 0; frameId < frames.length; ++frameId) {
-      var frame = frames[frameId];
-      //console.log(frame);
-      var colors = frame.Colors;
-      for (var i = 0; i < maxRow; ++i) {
-        var row = colors[i];
-        for (var j = 0; j < maxColumn; ++j) {
-          var color = row[j];
+    if (animation.DeviceType == EChromaSDKDeviceTypeEnum.DE_1D) {
+      var maxLeds = ChromaAnimation.getMaxLeds(animation.Device);
+      for (var frameId = 0; frameId < frames.length; ++frameId) {
+        var frame = frames[frameId];
+        //console.log(frame);
+        var colors = frame.Colors;
+        for (var i = 0; i < maxLeds; ++i) {
+          var color = colors[i];
           if (color != 0) {
             //console.log('color', color);
             var red = (color & 0xFF);
@@ -4731,7 +4724,33 @@ var ChromaAnimation = {
             green = Math.min(255, Math.max(0, Number(green) + Number(greenOffset))) & 0xFF;
             blue = Math.min(255, Math.max(0, Number(blue) + Number(blueOffset))) & 0xFF;
             color = red | (green << 8) | (blue << 16);
-            row[j] = color;
+            colors[i] = color;
+          }
+        }
+      }
+    } else if (animation.DeviceType == EChromaSDKDeviceTypeEnum.DE_2D) {
+      var maxRow = ChromaAnimation.getMaxRow(animation.Device);
+      var maxColumn = ChromaAnimation.getMaxColumn(animation.Device);
+      //console.log(animation.Frames);
+      for (var frameId = 0; frameId < frames.length; ++frameId) {
+        var frame = frames[frameId];
+        //console.log(frame);
+        var colors = frame.Colors;
+        for (var i = 0; i < maxRow; ++i) {
+          var row = colors[i];
+          for (var j = 0; j < maxColumn; ++j) {
+            var color = row[j];
+            if (color != 0) {
+              //console.log('color', color);
+              var red = (color & 0xFF);
+              var green = (color & 0xFF00) >> 8;
+              var blue = (color & 0xFF0000) >> 16;
+              red = Math.min(255, Math.max(0, Number(red) + Number(redOffset))) & 0xFF;
+              green = Math.min(255, Math.max(0, Number(green) + Number(greenOffset))) & 0xFF;
+              blue = Math.min(255, Math.max(0, Number(blue) + Number(blueOffset))) & 0xFF;
+              color = red | (green << 8) | (blue << 16);
+              row[j] = color;
+            }
           }
         }
       }
